544f9e120dd87d26470a7b18bf0baf2f248478ba
Daniel Suárez García <dsuarezg@uoc.edu>
2025-05-06 16:26:08 +0200

    Tasks Entities, Hierarchy and Relations Created

--- src/main/java/com/ironhack/TaskManager/models/UserTask.java
+++ src/main/java/com/ironhack/TaskManager/models/UserTask.java
@@ -1,4 +1,32 @@
 package com.ironhack.TaskManager.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "task_id", unique = true)
+    private Task task;
+
+    private String comments;
+
+
+
 }
